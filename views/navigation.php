f9a49445ce123961535573f3303b779bb52e62bc
root <root@DESKTOP-2TKMD5S.localdomain>
2017-12-06 11:42:40 +0100

    Updated links

--- views/navigation.php
+++ views/navigation.php
@@ -9,12 +9,11 @@
           <a class="nav-link" href="/about.php">About</a>
       </li><!-- /nav-item -->
       <li class="nav-item">
-          <a class="nav-link" href="/login.php">Login</a>
+          <?php if (isset($_SESSION['user'])): ?>
+              <a class="nav-link" href="/app/auth/logout.php"><?php echo "Logout" ?></a>
+          <?php else : ?>
+              <a class="nav-link" href="/login.php">Login</a>
+          <?php endif; ?>
       </li><!-- /nav-item -->
-      <?php if (isset($_SESSION['user'])): ?>
-          <li class="nav-item">
-              <a class="nav-link" href="/login.php">Logout</a>
-          </li><!-- /nav-item -->
-      <?php endif; ?>
   </ul><!-- /navbar-nav -->
 </nav><!-- /navbar -->
