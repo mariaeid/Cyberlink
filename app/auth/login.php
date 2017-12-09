fb251af601734ea7cefe8031b5fabd9253d57bf5
root <root@DESKTOP-2TKMD5S.localdomain>
2017-12-09 08:30:11 +0100

    Hash password

--- app/auth/login.php
+++ app/auth/login.php
@@ -9,6 +9,7 @@ require __DIR__.'/../autoload.php';
 if (isset($_POST['email'], $_POST['password'])) {
     $email = filter_var(trim($_POST['email']), FILTER_VALIDATE_EMAIL);
     $password = $_POST['password'];
+    $hash = password_hash($password, PASSWORD_DEFAULT);
 
     $pdo = new PDO('sqlite:../database/database.db');
     $statement = $pdo -> prepare("SELECT * FROM users where email = :email");
@@ -17,15 +18,27 @@ if (isset($_POST['email'], $_POST['password'])) {
     $statement -> execute();
     $userData = $statement -> fetch(PDO::FETCH_ASSOC);
 
+    //Redirecting back to login page if the email address doesn't exist
     if (empty($userData)) {
         redirect('/login.php');
     }
+    //If the email address exists and the password match we store the user details in a session and redirects the user to the start page
     else {
-        if (password_verify($password, $userData['password'])) {
-            $_SESSION['user'] = ["id" => $userData['id'], "name" => $userData['name'], "email" => $userData['email']];
+        if (password_verify($password, $userData['password']))
+        {
+            $_SESSION['user'] = [
+                "id" => $userData['id'],
+                "firstname" => $userData['firstname'],
+                "lastname" => $userData['lastname'],
+                "email" => $userData['email'],
+                "username" => $userData['username'],
+                "bio" => $userData['bio'],
+                "picture" => $userData['picture']
+            ];
 
             redirect('/index.php');
         }
+        //Redirecting back to login page if the password and email doesn't match
         else {
             redirect('/login.php');
         }
