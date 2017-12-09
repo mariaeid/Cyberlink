350b66b951794b2ec4cc35e5eac267b35368dd0c
root <root@DESKTOP-2TKMD5S.localdomain>
2017-12-09 08:32:20 +0100

    Updated email placeholder and describing text

--- login.php
+++ login.php
@@ -6,14 +6,14 @@
     <form action="app/auth/login.php" method="post">
         <div class="form-group">
             <label for="email">Email</label>
-            <input class="form-control" type="email" name="email" placeholder="francis@darjeeling.com" required>
-            <small class="form-text text-muted">Please provide the your email address.</small>
+            <input class="form-control" type="email" name="email" placeholder="harry.potter@hogwarts.com" required>
+            <small class="form-text text-muted">Please provide your email address</small>
         </div><!-- /form-group -->
 
         <div class="form-group">
             <label for="password">Password</label>
             <input class="form-control" type="password" name="password" required>
-            <small class="form-text text-muted">Please provide the your password (passphrase).</small>
+            <small class="form-text text-muted">Please provide your password</small>
         </div><!-- /form-group -->
 
         <button type="submit" class="btn btn-primary">Login</button>
