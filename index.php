4aedcdcfd426d2452207572f151c06263c3b22af
root <root@DESKTOP-2TKMD5S.localdomain>
2017-12-13 08:35:04 +0100

    Welcomeing name for logged in user

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 
 <article>
     <h1><?php echo $config['title']; ?></h1>
-    <p>This is the home page.</p>
+    <?php if (!isset($_SESSION['user'])): ?>
+        <p>This is the home page.</p>
+    <?php else : ?>
+        <p>Welcome <?php echo $_SESSION['user']['firstname'].' '.$_SESSION['user']['lastname'] ?></p>
+    <?php endif; ?>
 </article>
 
 <?php require __DIR__.'/views/footer.php'; ?>
