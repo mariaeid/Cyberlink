9bf811191b9c0d0d8481e18cc68aa3c431feee9f
root <root@DESKTOP-2TKMD5S.localdomain>
2017-12-13 08:36:06 +0100

    Comments

--- app/auth/add.php
+++ app/auth/add.php
@@ -6,6 +6,7 @@ require __DIR__.'/../autoload.php';
 
 // Creation of new user
 // FRÅGA: Bör man kolla empty fields trots att det är gjort frontend??
+// OBS! Lägg in koll så att användaren inte redan finns!
 
 if (isset($_POST['firstname'], $_POST['lastname'], $_POST['email'], $_POST['username'], $_POST['password'])) {
   $firstname = filter_var(trim($_POST['firstname']), FILTER_SANITIZE_STRING);
@@ -16,18 +17,20 @@ if (isset($_POST['firstname'], $_POST['lastname'], $_POST['email'], $_POST['user
 
   $statement = $pdo->prepare("INSERT INTO users (firstname, lastname, email, username, password) VALUES (:firstname, :lastname, :email, :username, :password)");
 
-  $statement->bindParam(':firstname', $firstname, PDO::PARAM_STR);
-  $statement->bindParam(':lastname', $lastname, PDO::PARAM_STR);
-  $statement->bindParam(':email', $email, PDO::PARAM_STR);
-  $statement->bindParam(':username', $username, PDO::PARAM_STR);
-  $statement->bindParam(':password', $password, PDO::PARAM_STR);
-
   if (!$statement) {
     die(var_dump(
         $pdo->errorInfo()
     ));
   }
 
-  redirect('/index.php');
+  $statement->bindParam(':firstname', $firstname, PDO::PARAM_STR);
+  $statement->bindParam(':lastname', $lastname, PDO::PARAM_STR);
+  $statement->bindParam(':email', $email, PDO::PARAM_STR);
+  $statement->bindParam(':username', $username, PDO::PARAM_STR);
+  $statement->bindParam(':password', $password, PDO::PARAM_STR);
+
+  $statement -> execute();
+
+  redirect('/login.php');
 
 }
