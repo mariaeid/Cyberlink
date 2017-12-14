96eb30dc9dc4745603510285c2c18b569fa065d1
root <root@DESKTOP-2TKMD5S.localdomain>
2017-12-14 16:37:27 +0100

    Adding changes on user to database

--- app/auth/edit.php
+++ app/auth/edit.php
@@ -4,23 +4,51 @@ declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
 
-// Edit of user profile details
+//Redirection to edit profile page
+if (isset($_POST['profile'])) {
+    redirect('/profileEdit.php');
+}
 
-if (isset($_POST['edit'])) {
+//Edit of user profile details
+
+if (isset($_POST['firstname'], $_POST['lastname'], $_POST['email'],$_POST['username'], $_POST['bio'])) {
     $firstname = filter_var(trim($_POST['firstname']), FILTER_SANITIZE_STRING);
     $lastname = filter_var(trim($_POST['lastname']), FILTER_SANITIZE_STRING);
     $email = filter_var(trim($_POST['email']), FILTER_VALIDATE_EMAIL);
     $username = filter_var(trim($_POST['username']), FILTER_SANITIZE_STRING);
     $bio = filter_var($_POST['bio'], FILTER_SANITIZE_STRING);
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    // echo $firstname. ' '.$lastname. ' '.$email.' '.$username.' '.$bio;
-
-    $statement = $pdo->prepare("UPDATE users SET firstname, lastname, email, username, password) VALUES (:firsname, :lastname, :email, :username, :password)");
+    $statement = $pdo->prepare("UPDATE users SET firstname = :firstname, lastname = :lastname, email = :email, username = :username, bio = :bio WHERE id = :id");
 
     if (!$statement) {
       die(var_dump(
           $pdo->errorInfo()
       ));
     }
+
+    $statement->bindParam(':id', $_SESSION['user']['id'], PDO::PARAM_INT);
+    $statement->bindParam(':firstname', $firstname, PDO::PARAM_STR);
+    $statement->bindParam(':lastname', $lastname, PDO::PARAM_STR);
+    $statement->bindParam(':email', $email, PDO::PARAM_STR);
+    $statement->bindParam(':username', $username, PDO::PARAM_STR);
+    $statement->bindParam(':bio', $bio, PDO::PARAM_STR);
+
+    $statement->execute();
+
+    $newData = $pdo->prepare("SELECT * FROM users where id = :id");
+    $newData->bindParam(':id', $_SESSION['user']['id'], PDO::PARAM_INT);
+    $newData->execute();
+    $userData = $newData->fetch(PDO::FETCH_ASSOC);
+
+    $_SESSION['user'] = [
+        "id" => $userData['id'],
+        "firstname" => $userData['firstname'],
+        "lastname" => $userData['lastname'],
+        "email" => $userData['email'],
+        "username" => $userData['username'],
+        "bio" => $userData['bio'],
+        "picture" => $userData['picture'],
+    ];
+
+    redirect('../../profile.php');
 }
