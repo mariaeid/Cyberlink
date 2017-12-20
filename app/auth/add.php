086fc1128a052ead2fcca3d282b2424b905cf73e
root <root@DESKTOP-2TKMD5S.localdomain>
2017-12-20 10:11:41 +0100

    Removal of redirection

--- app/auth/add.php
+++ app/auth/add.php
@@ -5,56 +5,63 @@ declare(strict_types=1);
 require __DIR__.'/../autoload.php';
 
 // Creation of new user
-// FRÅGA: Bör man kolla empty fields trots att det är gjort frontend?? Ja
-// OBS! Lägg in koll så att användaren inte redan finns!
 
 if (isset($_POST['firstname'], $_POST['lastname'], $_POST['email'], $_POST['username'], $_POST['password'])) {
-  $firstname = filter_var(trim($_POST['firstname']), FILTER_SANITIZE_STRING);
-  $lastname = filter_var(trim($_POST['lastname']), FILTER_SANITIZE_STRING);
-  $email = filter_var(trim($_POST['email']), FILTER_VALIDATE_EMAIL);
-  $username = filter_var(trim($_POST['username']), FILTER_SANITIZE_STRING);
-  $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $firstname = filter_var(trim($_POST['firstname']), FILTER_SANITIZE_STRING);
+    $lastname = filter_var(trim($_POST['lastname']), FILTER_SANITIZE_STRING);
+    $email = filter_var(trim($_POST['email']), FILTER_VALIDATE_EMAIL);
+    $username = filter_var(trim($_POST['username']), FILTER_SANITIZE_STRING);
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-  $statement = $pdo->prepare("INSERT INTO users (firstname, lastname, email, username, password) VALUES (:firstname, :lastname, :email, :username, :password)");
+//   $query = $pdo->query('SELECT email FROM users');
+//   $query->execute();
+//   $userEmails = $query->fetchAll(PDO::FETCH_ASSOC);
+//
+//   foreach ($userEmails as $userEmail) {
+//       if ($userEmail['email'] === $email) {
+//           $emailExists = true;
+//       }
+//   }
+//
+// }
 
-  if (!$statement) {
-    die(var_dump(
-        $pdo->errorInfo()
-    ));
-  }
+    $statement = $pdo->prepare("INSERT INTO users (firstname, lastname, email, username, password) VALUES (:firstname, :lastname, :email, :username, :password)");
 
-  $statement->bindParam(':firstname', $firstname, PDO::PARAM_STR);
-  $statement->bindParam(':lastname', $lastname, PDO::PARAM_STR);
-  $statement->bindParam(':email', $email, PDO::PARAM_STR);
-  $statement->bindParam(':username', $username, PDO::PARAM_STR);
-  $statement->bindParam(':password', $password, PDO::PARAM_STR);
+    if (!$statement) {
+        die(var_dump(
+            $pdo->errorInfo()
+        ));
+    }
 
-  $statement -> execute();
+    $statement->bindParam(':firstname', $firstname, PDO::PARAM_STR);
+    $statement->bindParam(':lastname', $lastname, PDO::PARAM_STR);
+    $statement->bindParam(':email', $email, PDO::PARAM_STR);
+    $statement->bindParam(':username', $username, PDO::PARAM_STR);
+    $statement->bindParam(':password', $password, PDO::PARAM_STR);
 
-  if (!$user) {
-     $statement = $pdo->query('SELECT * FROM users WHERE  username= :username');
-     $statement->bindParam(':username', $username, PDO::PARAM_STR);
-     $statement->execute();
+    $statement -> execute();
 
+    if (!$user) {
+        $statement = $pdo->query('SELECT * FROM users WHERE  username= :username');
+        $statement->bindParam(':username', $username, PDO::PARAM_STR);
+        $statement->execute();
 
-     $userData = $statement->fetch(PDO::FETCH_ASSOC);
+        $userData = $statement->fetch(PDO::FETCH_ASSOC);
 
+        $_SESSION['user'] = [
+            "id" => $userData['id'],
+            "firstname" => $userData['firstname'],
+            "lastname" => $userData['lastname'],
+            "email" => $userData['email'],
+            "username" => $userData['username'],
+            "bio" => $userData['bio'],
+            "picture" => $userData['picture'],
+        ];
 
-     $_SESSION['user'] = [
-         "id" => $userData['id'],
-         "firstname" => $userData['firstname'],
-         "lastname" => $userData['lastname'],
-         "email" => $userData['email'],
-         "username" => $userData['username'],
-         "bio" => $userData['bio'],
-         "picture" => $userData['picture'],
-     ];
+        redirect('../../profile.php');
 
-     redirect('../../profile.php');
+    }
 
-
-   }
-
-  redirect('/login.php');
+    redirect('/login.php');
 
 }
