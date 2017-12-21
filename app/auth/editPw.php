ff4692c5c5e08cc4a1a31b75655ba89a02efe2b9
root <root@DESKTOP-2TKMD5S.localdomain>
2017-12-21 08:20:18 +0100

    Changing password

---
+++ app/auth/editPw.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__.'/../autoload.php';
+
+if (isset($_POST['currentPassword'], $_POST['newPassword'], $_POST['confirmPassword'])) {
+    $currentPassword = $_POST['currentPassword'];
+    $newPassword = $_POST['newPassword'];
+    $confirmPassword = $_POST['confirmPassword'];
+
+    $statement = $pdo->prepare("SELECT * FROM users where id = :id");
+
+    $statement->bindParam(':id', $_SESSION['user']['id'], PDO::PARAM_STR);
+    $statement->execute();
+    $user = $statement->fetch(PDO::FETCH_ASSOC);
+
+      if (password_verify($currentPassword, $user['password']))
+      {
+          if ($newPassword === $confirmPassword) {
+            echo "correct";
+            $password = password_hash($newPassword, PASSWORD_DEFAULT);
+
+            $statement = $pdo->prepare("UPDATE users SET password = :password WHERE id = :id");
+
+              if (!$statement) {
+                  die(var_dump(
+                      $pdo->errorInfo()
+                  ));
+              }
+
+              $statement->bindParam(':id', $_SESSION['user']['id'], PDO::PARAM_INT);
+              $statement->bindParam(':password', $password, PDO::PARAM_STR);
+
+              $statement->execute();
+
+              $newData = $pdo->prepare("SELECT * FROM users where id = :id");
+              $newData->bindParam(':id', $_SESSION['user']['id'], PDO::PARAM_INT);
+              $newData->execute();
+              $user = $newData->fetch(PDO::FETCH_ASSOC);
+
+              unset($user['password']);
+
+              $_SESSION['user'] = $user;
+
+              redirect('../../profile.php');
+            }
+      }
+
+      else {
+          echo "Not correct";
+      }
+
+}
+
+
+// Försök att verifiera lösen
+// if (isset($_POST['currentPassword'])) {
+//     $currentPassword = password_hash($_POST['currentPassword'], PASSWORD_DEFAULT);
+//
+//     $statement = $pdo->prepare("SELECT * FROM users where id = :id");
+//     $statement->bindParam(':id', $_SESSION['user']['id'], PDO::PARAM_INT);
+//     $statement->execute();
+//     $userData = $statement->fetch(PDO::FETCH_ASSOC);
+//
+//     $oldPassword = $userData['password'];
+//
+//     var_dump($oldPassword);
+//     var_dump($currentPassword);
+//
+//     if(password_verify($oldPassword, $currentPassword)) {
+//       echo "Correct";
+//     }
+// }
---
+++ changePw.php
@@ -0,0 +1,26 @@
+<?php require __DIR__.'/views/header.php'; ?>
+
+<article>
+    <h1>Change Password</h1>
+
+    <form action="app/auth/editPw.php" method="post">
+
+        <div class="form-group">
+            <label for="currentPassword">Current password:</label>
+            <input class="form-control" type="password" name="currentPassword" value="<?php ?>" required>
+        </div><!-- /form-group -->
+        <div class="form-group">
+            <label for="newPassword">New password:</label>
+            <input class="form-control" type="password" name="newPassword" value="<?php ?>" required>
+        </div><!-- /form-group -->
+        <div class="form-group">
+            <label for="confirmPassword">Confirm new password:</label>
+            <input class="form-control" type="password" name="confirmPassword" value="<?php ?>" required>
+        </div><!-- /form-group -->
+
+        <button type="submit" name="editPw" class="btn btn-primary">Save Changes</button>
+    </form>
+
+</article>
+
+<?php require __DIR__.'/views/footer.php'; ?>
