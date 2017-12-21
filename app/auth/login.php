e913117d4d28460160a93e8eff6e5406726f2d36
root <root@DESKTOP-2TKMD5S.localdomain>
2017-12-21 08:18:09 +0100

    New way of saving session user

--- app/auth/login.php
+++ app/auth/login.php
@@ -9,32 +9,24 @@ require __DIR__.'/../autoload.php';
 if (isset($_POST['email'], $_POST['password'])) {
     $email = filter_var(trim($_POST['email']), FILTER_VALIDATE_EMAIL);
     $password = $_POST['password'];
-    $hash = password_hash($password, PASSWORD_DEFAULT);
 
     $statement = $pdo->prepare("SELECT * FROM users where email = :email");
-
     $statement->bindParam(':email', $email, PDO::PARAM_STR);
     $statement->execute();
-    $userData = $statement->fetch(PDO::FETCH_ASSOC);
+
+    $user = $statement->fetch(PDO::FETCH_ASSOC);
 
     //Redirecting back to login page if the email address doesn't exist
-    if (empty($userData)) {
+    if (empty($user)) {
         redirect('/login.php');
     }
     //If the email address exists and the password match we store the user details in a session and redirects the user to the start page
     else {
-        if (password_verify($password, $userData['password']))
+        if (password_verify($password, $user['password']))
         {
-            $_SESSION['user'] = [
-                "id" => $userData['id'],
-                "firstname" => $userData['firstname'],
-                "lastname" => $userData['lastname'],
-                "email" => $userData['email'],
-                "username" => $userData['username'],
-                "bio" => $userData['bio'],
-                "picture" => $userData['picture'],
-                "password" => $userData['password'],
-            ];
+            unset($user['password']);
+
+            $_SESSION['user'] = $user;
 
             redirect('/index.php');
         }
