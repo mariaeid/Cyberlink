f5f3d134a31f60a9b2064becfcd84b97847035c1
root <root@DESKTOP-2TKMD5S.localdomain>
2018-01-08 16:51:24 +0100

    Saves email address if wrong pw

--- login.php
+++ login.php
@@ -1,19 +1,31 @@
-<?php require __DIR__.'/views/header.php'; ?>
+<?php
+require __DIR__.'/views/header.php';
+require __DIR__.'/app/auth/login.php';
+?>
 
 <article>
     <h1>Login</h1>
 
-    <form action="app/auth/login.php" method="post">
+    <form action="login.php" method="post">
         <div class="form-group">
             <label for="email">Email</label>
-            <input class="form-control" type="email" name="email" placeholder="harry.potter@hogwarts.com" required>
+            <input class="form-control" type="email" name="email" required
+            value=<?php if (isset($_POST['email'])): ?>
+                "<?php echo $email ?>"
+            <?php endif; ?>>
             <small class="form-text text-muted">Please provide your email address</small>
+            <?php if ($errorEmail): ?>
+                <p class="alert alert-danger"><?php echo $errorEmail; ?></p>
+            <?php endif; ?>
         </div><!-- /form-group -->
 
         <div class="form-group">
             <label for="password">Password</label>
             <input class="form-control" type="password" name="password" required>
             <small class="form-text text-muted">Please provide your password</small>
+            <?php if ($errorPw): ?>
+                <p class="alert alert-danger"><?php echo $errorPw; ?></p>
+            <?php endif; ?>
         </div><!-- /form-group -->
 
         <button type="submit" class="btn btn-primary">Login</button>
