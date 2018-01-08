a91d559d4039e809207dd8d9ffbe5e19991eef1d
root <root@DESKTOP-2TKMD5S.localdomain>
2018-01-08 16:57:44 +0100

    Added cancel functionality

--- profileEdit.php
+++ profileEdit.php
@@ -7,30 +7,31 @@
 
         <div class="form-group">
             <label for="firstname">First Name:</label>
-            <input class="form-control" type="text" name="firstname" value="<?php echo $_SESSION['user']['firstname'];?>">
+            <input class="form-control" type="text" name="firstname" value="<?php echo $_SESSION['user']['firstname'];?>" required>
         </div><!-- /form-group -->
 
         <div class="form-group">
             <label for="lastname">Last Name:</label>
-            <input class="form-control" type="text" name="lastname" value="<?php echo $_SESSION['user']['lastname'];?>">
+            <input class="form-control" type="text" name="lastname" value="<?php echo $_SESSION['user']['lastname'];?>" required>
         </div><!-- /form-group -->
 
         <div class="form-group">
             <label for="lastname">Email:</label>
-            <input class="form-control" type="text" name="email" value="<?php echo $_SESSION['user']['email'];?>">
+            <input class="form-control" type="text" name="email" value="<?php echo $_SESSION['user']['email'];?>" required>
         </div><!-- /form-group -->
 
         <div class="form-group">
             <label for="username">Username: </label>
-            <input class="form-control" type="text" name="username" value="<?php echo $_SESSION['user']['username'];?>">
+            <input class="form-control" type="text" name="username" value="<?php echo $_SESSION['user']['username'];?>" required>
         </div><!-- /form-group -->
 
         <div class="form-group">
             <label for="lastname">Bio:</label>
-            <textarea name="bio" rows="8" cols="80"><?php echo $_SESSION['user']['bio'];?></textarea>
+            <textarea class="form-control" name="bio" rows="8" cols="80"><?php echo $_SESSION['user']['bio'];?></textarea>
         </div><!-- /form-group -->
 
         <button type="submit" name="edit" class="btn btn-primary">Save Changes</button>
+        <button type="submit" name="cancel" class="btn btn-primary">Cancel</button>
     </form>
 </article>
 
