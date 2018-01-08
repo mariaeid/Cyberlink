7d5c5e4af6eadfcb73379c8ddd4edbfe1161c182
root <root@DESKTOP-2TKMD5S.localdomain>
2018-01-08 16:53:18 +0100

    Shows if user have voted

--- index.php
+++ index.php
@@ -4,43 +4,100 @@ require __DIR__.'/app/posts/store.php';
 require __DIR__.'/app/posts/vote.php';
 ?>
 
-<article>
-    <h1><?php echo $config['title']; ?></h1>
-    <?php if (isset($_SESSION['user'])): ?>
-        <p>Welcome <?php echo $_SESSION['user']['firstname'].' '.$_SESSION['user']['lastname'] ?></p>
-        <form action="/newSubmission.php" method="post">
-            <div class="form-group">
-                <button type="submit" name="newLink" class="btn btn-primary">Add link</button>
-            </div>
-        </form>
-    <?php endif; ?>
-</article>
+<div class="container-fluid">
+    <div class="row welcome">
+        <div class="col-md-10 py-2">
+            <h3>Welcome to <?php echo $config['title']; ?></h3>
+            <h6 class="py-3">Find and share your best links.</h6>
+        </div>
+        <div class="col-md-2">
+            <?php if (isset($_SESSION['user'])): ?>
+                <p class="pull-right">Logged in as: <?php echo $_SESSION['user']['username']; ?></p>
+                <?php if (!isset($_SESSION['user']['picture'])): ?>
+                    <img src="app/imgs/avatar_placeholder.png" class="img-thumbnail rounded-circle avatarSmall pull-right">
+                <?php else : ?>
+                    <img src="app/imgs/<?php echo $_SESSION['user']['picture']?>" class="img-thumbnail rounded-circle avatarSmall pull-right">
+                <?php endif; ?>
+            <?php endif; ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <?php if (isset($_SESSION['user'])): ?>
+                <form action="/newSubmission.php" method="post">
+                    <div class="form-group">
+                        <button type="submit" name="newLink" class="btn btn-light">Add a new post</button>
+                    </div>
+                </form>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
+
 <article>
     <?php foreach ($posts as $post): ?>
-        <form action="/app/posts/vote.php" method="post">
-            <div class="form-group border border-info p-3">
-                <a href="<?php echo $post['url']; ?>" target="_blank"><?php echo $post['title']; ?></a>
+        <form action="index.php#<?php echo $post['title']; ?>" id="<?php echo $post['title']?>" method="post">
+            <div class="border border-info p-3 m-3">
+                <a id="<?php echo $post['title']?>" class="siteLink" href="<?php echo $post['url']; ?>" target="_blank"><?php echo $post['title']; ?></a>
                 <p><?php echo $post['description']; ?></p>
-                <p>Submitted by <?php echo $post['username']; ?></p>
-                <?php if (isset($_SESSION['user'])):?>
-                    <button class="fa fa-thumbs-o-up" aria-hidden="true" name="up"></button>
-                    <?php
+                <p class="fontSmall"> Submitted by <?php echo $post['username']; ?></p>
+                <div class="voteContainer">
+                    <div class="upVotes">
+                        <!-- Count of all upvotes on the post -->
+                        <?php
                         $queryUpVotes = $pdo->query('SELECT COUNT(*) FROM votes WHERE direction="1" AND post_id = :post_id');
                         $queryUpVotes->bindParam(':post_id', $post['id'], PDO::PARAM_STR);
                         $queryUpVotes->execute();
                         $upVotes = $queryUpVotes->fetch(PDO::FETCH_ASSOC);
-                    ?>
-                    <p><?php echo $upVotes['COUNT(*)']; ?></p>
-                    <button class="fa fa-thumbs-o-down" aria-hidden="true" name="down"></button>
-                    <?php
+                        ?>
+                        <p class="voteNumber pr-1"><?php echo $upVotes['COUNT(*)']; ?></p>
+                        <!-- Count on all upvotes on the post by the signed in user -->
+                        <?php
+                        $queryUpVotesUser = $pdo->query('SELECT * FROM votes WHERE direction="1" AND post_id = :post_id AND username = :username');
+                        $queryUpVotesUser->bindParam(':post_id', $post['id'], PDO::PARAM_STR);
+                        $queryUpVotesUser->bindParam(':username', $_SESSION['user']['username'], PDO::PARAM_STR);
+                        $queryUpVotesUser->execute();
+                        $upVotesUser = $queryUpVotesUser->fetch(PDO::FETCH_ASSOC);
+                        ?>
+                        <?php if (isset($_SESSION['user'])): ?>
+                            <?php if (!empty($upVotesUser)): ?>
+                                <button class="fa fa-thumbs-o-up fa-flag voteButton voteButtonClicked" aria-hidden="true" name="up" onsubmit=""></button>
+                                <?php else: ?>
+                                <button class="fa fa-thumbs-o-up fa-flag voteButton" aria-hidden="true" name="up" onsubmit=""></button>
+                            <?php endif; ?>
+                        <?php else: ?>
+                            <i class="fa fa-thumbs-o-up fa-lg fa-pull-righ" aria-hidden="true"></i>
+                        <?php endif ; ?>
+                    </div>
+                    <div class="downVotes">
+                        <!-- Count of all upvotes on the post -->
+                        <?php
                         $queryDownVotes = $pdo->query('SELECT COUNT(*) FROM votes WHERE direction="-1" AND post_id = :post_id');
                         $queryDownVotes->bindParam(':post_id', $post['id'], PDO::PARAM_STR);
                         $queryDownVotes->execute();
                         $downVotes = $queryDownVotes->fetch(PDO::FETCH_ASSOC);
-                    ?>
-                    <p><?php echo $downVotes['COUNT(*)']; ?></p>
-                    <input type="hidden" name="post_id" value="<?php echo $post['id']; ?>">
-                <?php endif; ?>
+                        ?>
+                        <p class="voteNumber pr-1"><?php echo $downVotes['COUNT(*)']; ?></p>
+                        <!-- Count on all downvotes on the post by the signed in user -->
+                        <?php
+                        $queryDownVotesUser = $pdo->query('SELECT * FROM votes WHERE direction="-1" AND post_id = :post_id AND username = :username');
+                        $queryDownVotesUser->bindParam(':post_id', $post['id'], PDO::PARAM_STR);
+                        $queryDownVotesUser->bindParam(':username', $_SESSION['user']['username'], PDO::PARAM_STR);
+                        $queryDownVotesUser->execute();
+                        $downVotesUser = $queryDownVotesUser->fetch(PDO::FETCH_ASSOC);
+                        ?>
+                        <?php if (isset($_SESSION['user'])): ?>
+                            <?php if (!empty($downVotesUser)): ?>
+                                <button class="fa fa-thumbs-o-down fa-flag voteButton voteButtonClicked" aria-hidden="true" name="down" onsubmit=""></button>
+                                <?php else: ?>
+                                <button class="fa fa-thumbs-o-down fa-flag voteButton" aria-hidden="true" name="down" onsubmit=""></button>
+                            <?php endif; ?>
+                        <?php else: ?>
+                            <i class="fa fa-thumbs-o-up fa-lg fa-pull-righ" aria-hidden="true"></i>
+                        <?php endif ; ?>
+                    </div>
+                </div>
+                <input type="hidden" name="post_id" value="<?php echo $post['id']; ?>">
             </div>
         </form>
     <?php endforeach; ?>
